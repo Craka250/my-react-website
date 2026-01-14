36600d24e3b1643b8f7bbc44cfe0a204d6c7d68d
Ayu_bu <otienojobochieng19@gmail.com>
2026-01-14 06:56:17 -0800

    Imported the page componets

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -2,6 +2,10 @@ import Header from "./Header.tsx";
 import Footer from "./Footer.tsx";
 import Food from "./Food.tsx";
 import Card from "./Card.tsx";
+import Home from "./Home.tsx";
+import About from "./About.tsx";
+import Contact from "./Contact.tsx";
+import Services from "./Services.tsx";
 
 
 function App() {
@@ -9,9 +13,13 @@ function App() {
   return (
 
 	<div> {/* Used for fragment-like wrapper */}
-	  <Header />
-    <Food />
+	  <Home />
+    <Header />
+    <About />
     <Card />
+    <Services />
+    <Contact />
+    <Food />
     <Footer />
 	</div>
 
